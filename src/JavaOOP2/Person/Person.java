a29db94c467e4b2113f563c89220ea78ef3a53db
nquynhhhhhhh <phamnhuquynh13122002@gmail.com>
2025-08-09 20:50:22 +0700

    sửa bt javaoop

--- src/JavaOOP2/Person/Person.java
+++ src/JavaOOP2/Person/Person.java
@@ -1,14 +1,16 @@
 package JavaOOP2.Person;
 
 public class Person {
-    private String name;
-    private int age;
-    private String gender;
-    private String address;
-    private String phone;
+    public String name;
+    public int age;
+    public String gender;
+    //để protected để bên package khác kh gọi đc
+    protected String address;
+    protected String phone;
+    //=> kh được tạo hàm public void rồi sout vì khác phạm vi truy cập
+    //=> tách biệt ra dùng get, set để phân biệt ra
 
-
-    public Person (String name,int age,String gender,String address,String phone){
+    public Person(String name, int age, String gender, String address, String phone) {
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -16,52 +18,44 @@ public class Person {
         this.phone = phone;
     }
 
-    public Person (String name1,int age1,String gender1){
+    public Person(String name1, int age1, String gender1) {
         name = name1;
         age = age1;
         gender = gender1;
     }
 
+    //ở đây kh cần hàm get vẫn được nhưng có sẽ hay hơn
     public String getName() {
         return name;
     }
+
     public int getAge() {
         return age;
     }
+
     public String getGender() {
         return gender;
     }
-    public String getAddress() {
-        return address;
-    }
 
-    public String getPhone() {
+    protected String getAddress() {return address;}
+
+    protected String getPhone() {
         return phone;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-//    public void setPhone(int phone) {
-//        this.phone = phone;
-//    }
+//    public void setName(String name) {this.name = name;}
+//    public void setAge(int age) {this.age = age;}
+//    public void setGender(String gender) {this.gender = gender;}
+//    public void setAddress(String address) {this.address = address;}
+//    public void setPhone(int phone) {this.phone = phone;}
 
-    }
+}
 
-    //public static void main(String[] args) {
-        //Person nhanluc = new Person();
-    //}
-    //=> KHÔNG THỂ KHỞI TẠO CHÍNH NÓ BÊN TRONG CHÍNH CLASS PERSON ĐƯỢC
-    // vì như vậy sẽ tạo vòng lặp vô hạn (gọi constructor)
+//public static void main(String[] args) {
+//Person nhanluc = new Person();
+//}
+//=> KHÔNG THỂ KHỞI TẠO CHÍNH NÓ BÊN TRONG CHÍNH CLASS PERSON ĐƯỢC
+// vì như vậy sẽ tạo vòng lặp vô hạn (gọi constructor)
 
 
 
--- src/JavaOOP3/common/BaseTest.java
+++ src/JavaOOP3/common/BaseTest.java
@@ -4,13 +4,25 @@ public class BaseTest{
 
 
     public static void createDriver() {
-        System.out.println("Browser: " + Constants.browser);
-        System.out.println("Report: " + Constants.report);
-        System.out.println("Headless: " + Constants.headless);
+        System.out.println("Project Name: " + Constants.PROJECT_NAME);
+        System.out.println("Browser: " + Constants.getBrowser());
+        System.out.println("Report: " + Constants.isReport());
+        System.out.println("Headless: " + Constants.isHeadless());
     }
 
-    public static void closeDriver() {
-        System.out.println("Closed browser: " + Constants.browser);
+    public static void createDriver(String browser) {
+        System.out.println("Browser: " + browser);
+        System.out.println("Report: " + Constants.isReport());
+        System.out.println("Headless: " + Constants.isHeadless());
+    }
+
+    //Cách 1: thể hiện tính đa hình => truyền giá trị dynamic
+    public static void closeDriver(String browser) { //truyền browser
+        System.out.println("Closed browser: " + browser);
+    }
+    //Cách 2: hàm kh truyền giá trị thì lấy từ vị trí chung, giá trị cứng
+    public static void closeDriver() { //truyền browser
+        System.out.println("Closed browser: " + Constants.getBrowser());
     }
 
     public static void main(String[] args) {
